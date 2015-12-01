ccc347e33bb6c32cca2fb8e015e1d53f6aa44118
Sarathi <nyssri@gmail.com>
2015-12-01 12:00:28 -0500

    new stuff

---
+++ menu_app/cuisines.js
@@ -0,0 +1,12 @@
+// 1. set the button status to unselected
+// 2. create a click event that will identify when the button is clicked
+// 3. when clicked, if the button status is notAdded then set the text to 'Add'
+// 4. when clicked, if the button status is Added, then set the text to 'Remove'
+// 5. save selected to cuisines to an object?  would need a dynamic object because won't know how many cuisines will be selected
+
+// var buttonStatus = 'notAdded'
+button.addEventListener("click", function(){
+	if buttonStatus = 'notAdded' {//change the the status to Added and change the text to Remove
+		get
+	} 
+}
