0147be9e7935ef1fc5143015d9cd66ec39ea4997
Sarathi <nyssri@gmail.com>
2015-12-02 12:42:34 -0500

    menu app

--- menu_app/cuisines.js
+++ menu_app/cuisines.js
@@ -4,9 +4,82 @@
 // 4. when clicked, if the button status is Added, then set the text to 'Remove'
 // 5. save selected to cuisines to an object?  would need a dynamic object because won't know how many cuisines will be selected
 
-// var buttonStatus = 'notAdded'
-button.addEventListener("click", function(){
-	if buttonStatus = 'notAdded' {//change the the status to Added and change the text to Remove
-		get
-	} 
-}
+// First jquery attempt --------
+// $(document).ready(function()
+//   {
+//   	$('button').click(function()
+//   	{
+//   		if ($(this).value = "Add")
+//   			{
+//   				$(this).value = "Remove";
+//   			}
+//   		else
+//   			{
+//   				$(this).value = "Add";
+//   			}
+//   	}):
+//   });
+
+//Second jquery attempt ------
+// $(document).ready(function(){
+// 	$("#mexican")."click"(function()
+// 		{
+// 			alert.("Button clicked");
+// 		}
+// 	);
+// });
+
+
+//Javascript by ID------------
+
+var buttonMexican = document.getElementById("mexican")
+var buttonMexicanValue = buttonMexican.textContent
+
+buttonMexican.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
+	if (buttonMexicanValue === "Add")
+	{
+		buttonMexican.textContent = "Remove";
+		buttonMexicanValue = "Remove";
+	}
+	else
+	{
+		buttonMexican.textContent = "Add";
+		buttonMexicanValue = "Add";
+	}
+});
+
+var buttonIndian = document.getElementById("indian")
+var buttonIndianValue = buttonIndian.textContent
+
+buttonIndian.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
+	if (buttonIndianValue === "Add")
+	{
+		buttonIndian.textContent = "Remove";
+		buttonIndianValue = "Remove";
+	}
+	else
+	{
+		buttonIndian.textContent = "Add";
+		buttonIndianValue = "Add";
+	}
+});
+
+var buttonItalian = document.getElementById("italian")
+var buttonItalianValue = buttonItalian.textContent
+
+buttonItalian.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
+	if (buttonItalianValue === "Add")
+	{
+		buttonItalian.textContent = "Remove";
+		buttonItalianValue = "Remove";
+	}
+	else
+	{
+		buttonItalian.textContent = "Add";
+		buttonItalianValue = "Add";
+	}
+});
+
+
+
+
