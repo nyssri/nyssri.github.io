1de6a884f13e0d6038008f51e6d546d63c0322fb
Sarathi <nyssri@gmail.com>
2015-12-07 11:21:33 -0500

    home page

--- menu_app/cuisines.js
+++ menu_app/cuisines.js
@@ -5,74 +5,75 @@
 // 5. save selected to cuisines to an object?  would need a dynamic object because won't know how many cuisines will be selected
 
 // First jquery attempt --------
-// $(document).ready(function()
-//   {
-//   	$('button').on("click",function()
-//   	{
-//   		if ($(this).value = "Add")
-//   			{
-//   				$(this).value = "Remove";
-//   			}
-//   		else
-//   			{
-//   				$(this).value = "Add";
-//   			}
-//   	}):
-//   });
+$(document).ready(function()
+  {
+  	$('button').on("click",function()
+  	{
+  		if ($(this).value = "I Don't Like It")
+  			{
+  				// $(this).value = "I Like It";
+  				alert("Clicked")
+  			}
+  		else
+  			{
+  				$(this).value = "I Don't Like It";
+  			}
+  	}):
+  });
 
 
 
 
 //Javascript by ID------This actually works!!!------
 
-var buttonMexican = document.getElementById("mexican")
-var buttonMexicanValue = buttonMexican.textContent
+// var buttonMexican = document.getElementById("mexican")
+// var buttonMexicanValue = buttonMexican.textContent
 
-buttonMexican.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
-	if (buttonMexicanValue === "I Don't Like It")
-	{
-		buttonMexican.textContent = "I Like It";
-		buttonMexicanValue = "I Like It";
-	}
-	else
-	{
-		buttonMexican.textContent = "I Don't Like It";
-		buttonMexicanValue = "I Don't Like It";
-	}
-});
+// buttonMexican.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
+// 	if (buttonMexicanValue === "I Don't Like It")
+// 	{
+// 		buttonMexican.textContent = "I Like It";
+// 		buttonMexicanValue = "I Like It";
+// 	}
+// 	else
+// 	{
+// 		buttonMexican.textContent = "I Don't Like It";
+// 		buttonMexicanValue = "I Don't Like It";
+// 	}
+// });
 
-var buttonIndian = document.getElementById("indian")
-var buttonIndianValue = buttonIndian.textContent
+// var buttonIndian = document.getElementById("indian")
+// var buttonIndianValue = buttonIndian.textContent
 
-buttonIndian.addEventListener("click", function(e) { //syntax= variableName.addEventListener("eventType", function details)
-	console.log('e: ', e);
-	if (buttonIndianValue === "I Don't Like It")
-	{
-		buttonIndian.textContent = "I Like It";
-		buttonIndianValue = "I Like It";
-	}
-	else
-	{
-		buttonIndian.textContent = "I Don't Like It";
-		buttonIndianValue = "I Don't Like It";
-	}
-});
+// buttonIndian.addEventListener("click", function(e) { //syntax= variableName.addEventListener("eventType", function details)
+// 	console.log('e: ', e);
+// 	if (buttonIndianValue === "I Don't Like It")
+// 	{
+// 		buttonIndian.textContent = "I Like It";
+// 		buttonIndianValue = "I Like It";
+// 	}
+// 	else
+// 	{
+// 		buttonIndian.textContent = "I Don't Like It";
+// 		buttonIndianValue = "I Don't Like It";
+// 	}
+// });
 
-var buttonItalian = document.getElementById("italian")
-var buttonItalianValue = buttonItalian.textContent
+// var buttonItalian = document.getElementById("italian")
+// var buttonItalianValue = buttonItalian.textContent
 
-buttonItalian.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
-	if (buttonItalianValue === "I Don't Like It")
-	{
-		buttonItalian.textContent = "I Like It";
-		buttonItalianValue = "I Like It";
-	}
-	else
-	{
-		buttonItalian.textContent = "I Don't Like It";
-		buttonItalianValue = "I Don't Like It";
-	}
-});
+// buttonItalian.addEventListener("click", function() { //syntax= variableName.addEventListener("eventType", function details)
+// 	if (buttonItalianValue === "I Don't Like It")
+// 	{
+// 		buttonItalian.textContent = "I Like It";
+// 		buttonItalianValue = "I Like It";
+// 	}
+// 	else
+// 	{
+// 		buttonItalian.textContent = "I Don't Like It";
+// 		buttonItalianValue = "I Don't Like It";
+// 	}
+// });
 
 // 'e:' is a function that represents the click event.  Can use this to access innerHTML(?) poperty.  Use e.innerHTML instead 
 // of button ids to identify which element is being clicked.
