4589351dee7ce78ef4d080a31b21708075a73168
Sarathi <nyssri@gmail.com>
2016-02-16 12:20:18 -0500

    recipe click

---
+++ menu_app/recipes.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#btnRecipes').click(function() {
+		alert("Thanks for clicking but this is all I can do now.  Come back soon for actual recipes!");
+		//Make a call to the API and display the results
+		// var recipes;
+		// recipes = curl "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}"
+		// console.log(recipes);
+	});
+});
